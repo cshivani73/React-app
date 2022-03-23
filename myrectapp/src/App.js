6c7ebd27b2f7936660e3f256eac1ceb96fc91c25
Shivani <shivanichouhan697@gmail.com>
2022-03-24 03:42:01 +0530

    addCode

---
+++ myrectapp/src/App.js
@@ -0,0 +1,19 @@
+
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+                <p id="code">
+          Welcome  Shivani Chouhan
+        </p>
+        <button id="btn1">More</button>
+        <button id="btn2">View</button>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
